4bae2b5c4354bc297cbca214a999a16336ec8499
bohdandovbush <dovbushb@gmail.com>
2021-08-04 14:27:47 +0300

    Create entity

---
+++ src/main/java/com/example/test_task/entity/Words.java
@@ -0,0 +1,15 @@
+package com.example.test_task.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Words {
+
+    private String[] words;
+}
