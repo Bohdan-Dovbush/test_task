7712562489fac6e7dc331e6d8a04f403c4917d67
bohdandovbush <dovbushb@gmail.com>
2021-08-04 15:07:53 +0300

    Create controller for word game

---
+++ src/main/java/com/example/test_task/controller/TestTask.java
@@ -0,0 +1,27 @@
+package com.example.test_task.controller;
+
+import com.example.test_task.entity.Words;
+import com.example.test_task.service.WordsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/words")
+public class TestTask {
+
+    private final WordsService wordsService;
+
+    @Autowired
+    public TestTask(WordsService wordsService) {
+        this.wordsService = wordsService;
+    }
+
+    @PostMapping(value = "/game")
+    public Words game(@RequestBody Words jWords) {
+        wordsService.checkWords(jWords);
+        return jWords;
+    }
+}
